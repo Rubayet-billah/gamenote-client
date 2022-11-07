06488f824280046d0980802081033fb43fe0ee6e
Rubayet-billah <rubayetbillah909@gmail.com>
2022-11-08 01:22:28 +0600

    successfully data loaded in home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [games, setGames] = useState([])
+
+    const size = 3;
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/games/?size=${size}`)
+            .then(res => res.json())
+            .then(data => {
+                setGames(data)
+            })
+    }, [])
     return (
-        <div>
-            home
+        <div className='text-8xl'>
+            {games.length}
         </div>
     );
 };
