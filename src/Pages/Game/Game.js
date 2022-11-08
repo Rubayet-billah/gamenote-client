f55d0a6c8618039bbe23f6adffb0ca05c3c80c9a
Rubayet-billah <rubayetbillah909@gmail.com>
2022-11-09 03:26:55 +0600

    details added conditionally

--- src/Pages/Game/Game.js
+++ src/Pages/Game/Game.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import 'react-photo-view/dist/react-photo-view.css';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
 
 const Game = ({ game }) => {
 
@@ -9,10 +11,18 @@ const Game = ({ game }) => {
     return (
         <div>
             <div className="card bg-base-100 shadow-xl">
-                <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+                <figure>
+                    <PhotoProvider>
+                        <div className="foo">
+                            <PhotoView src='https://placeimg.com/400/225/arch'>
+                                <img src='https://placeimg.com/400/225/arch' alt="" />
+                            </PhotoView>
+                        </div>
+                    </PhotoProvider>
+                </figure>
                 <div className="card-body">
                     <h2 className="card-title">{game.name}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <p>{game.details.length > 100 ? <>{game.details.slice(0, 99)}...</> : game.details}</p>
                     <div className="card-actions justify-end">
                         <Link to={`/services/${game._id}`}><button className="btn btn-sm btn-primary">Details</button></Link>
                     </div>
