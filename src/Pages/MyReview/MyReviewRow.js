07d7bfd3fae217acb3cd4caa650c89d0af475921
Rubayet-billah <rubayetbillah909@gmail.com>
2022-11-08 21:49:21 +0600

    review delete implemented

--- src/Pages/MyReview/MyReviewRow.js
+++ src/Pages/MyReview/MyReviewRow.js
@@ -2,7 +2,32 @@ import React from 'react';
 import { FaStar, FaTrashAlt } from 'react-icons/fa';
 
 const MyReviewRow = ({ review }) => {
-    const { serviceName, reviewer, email, image, description, ratings } = review;
+    const { _id, serviceName, reviewer, email, image, description, ratings } = review;
+
+    const handleDelete = (id) => {
+        fetch(`http://localhost:5000/myreview/${id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+
+    const handleUpdate = (id) => {
+        fetch(`http://localhost:5000/myreview/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({ description: 'this is edited' })
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+
     return (
         <div>
             <div className='border rounded p-3 mt-3'>
@@ -17,8 +42,8 @@ const MyReviewRow = ({ review }) => {
                     <p className='w-3/4'>{description}</p>
                     <div>
                         <p className='flex items-center'>{ratings} <FaStar className='text-yellow-400' /></p>
-                        <button className='btn btn-sm btn-outline mr-2'>Edit</button> <br />
-                        <button className='btn btn-sm btn-outline btn-error mt-3'><FaTrashAlt className='mx-2' /></button>
+                        <button onClick={() => handleUpdate(_id)} className='btn btn-sm btn-outline mr-2'>Edit</button> <br />
+                        <button onClick={() => handleDelete(_id)} className='btn btn-sm btn-outline btn-error mt-3'><FaTrashAlt className='mx-2' /></button>
                     </div>
                 </div>
             </div>
